72ade2768dfd2037453fa7961905ecba00e8def7
Joshua Nash <joshuangola@gmail.com>
2023-01-17 18:43:51 -0700

    solved

--- maxOfTwo.js
+++ maxOfTwo.js
@@ -3,3 +3,10 @@ let y = 2
 
 // Write your code below this line.
 
+if (x > y) {
+  console.log(`${x} is greater than ${y}`);
+} else if (x < y) {
+  console.log(`${y} is greater than ${x}`);
+} else {
+  console.log(`${x} and ${y} are equal`);
+}
